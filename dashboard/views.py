299ea4fb27e3fce5633dcb3f2631d72b00eb6c46
julamifra <julamifra@gmail.com>
2023-03-11 19:23:58 +0000

    create booking list view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
-# Create your views here.
+
+class BookingList(generic.ListView):
+    model = Booking
+    queryset = Booking.objects.order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+    # queryset = Booking.objects.filter(status=1)
