793fdb3f1611c84312ade1ed1f3c99067e340ab1
julamifra <julamifra@gmail.com>
2023-03-11 14:21:45 +0000

    add summernote

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
