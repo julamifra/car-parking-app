88c1babfd41381c25b725164f34954707a516e1a
julamifra <julamifra@gmail.com>
2023-03-12 04:12:10 +0000

    clean code

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
-# STATUS = ((0, "Draft"), (1, "Published"))
 class Booking(models.Model):
     booking_name = models.TextField()
     author = models.ForeignKey(
