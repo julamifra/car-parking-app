318943ff6662e7745c37e57b0c12ef961c63f2da
julamifra <julamifra@gmail.com>
2023-04-25 17:42:15 +0000

    add edit booking functionality

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.BookingList.as_view(), name='home'),
     path('booking_creation/', views.BookingCreation.as_view(), name='booking_creation'),
     path('booking_edition/', views.BookingEdition.as_view(), name='booking_edition'),
+    path('booking_edition/<str:booking_id>', views.BookingEdition.as_view(), name='booking_edition'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -52,13 +52,48 @@ class BookingCreation(View):
 
 
 class BookingEdition(View):
-    def get(self, request, *args, **kwargs):
+
+    def get(self, request, booking_id, *args, **kwargs):
+
+        booking = get_object_or_404(Booking, id=booking_id)
 
         return render(
             request,
             "booking_edition.html",
             {
                 "created": False,
-                "booking_form": BookingForm()
+                "booking_form": BookingForm(data={
+                    'booking_name': booking.booking_name,
+                    'car_model': booking.car_model,
+                    'notes': booking.notes,
+                    'start_booking_date': booking.start_booking_date,
+                    'end_booking_date': booking.end_booking_date})
+            },
+        )
+
+    def post(self, request, *args, **kwargs):
+
+        queryset = User.objects.filter(username=request.user.username)
+        user = get_object_or_404(queryset)
+
+        booking_form = BookingForm(data=request.POST)
+
+        if booking_form.is_valid():
+            booking_form.instance.email = request.user.email
+            booking_form.instance.name = request.user.username
+
+            booking = booking_form.save(commit=False)
+            booking.author = user
+
+            booking.save()
+        else:
+            booking_form = BookingForm()
+
+        return render(
+            request,
+            "booking_edition.html",
+            {
+                "created": True,
+                "booking_form": BookingForm(data=request.POST)
             },
         )
